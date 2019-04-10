d3ce2947c5df12551a66f67a9a15f2de3fc80200
Taeyeob <21300443@handong.edu>
2019-04-10 14:00:02 +0900

    mail address linked

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -191,7 +191,7 @@
                 <v-card-text>
                 <div>All Icons are made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
                 <br>
-                Please contact through swjomojomo@gmail.com
+                Please contact through <a href="mailto:swjomojomo@gmail.com">swjomojomo@gmail.com</a>
                 </v-card-text>
             </v-card>
         </v-flex>
